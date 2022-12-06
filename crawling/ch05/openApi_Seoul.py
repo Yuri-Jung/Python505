cf4428fc855afc6e9422692b8dfcce91926bd801
Yuri-Jung <dbfl14433@gmail.com>
2022-12-06 16:31:09 +0900

    크롤링 서울

--- crawling/ch05/openApi_Seoul.py
+++ crawling/ch05/openApi_Seoul.py
@@ -22,7 +22,15 @@ def getRequestUrl(url):
         print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
         return None
 
-#[CODE 2] 서울 열린데이터 광장 : 한 page에 5개만 display된다
+# [CODE 2] '서울 열린테이터 광장 : 한 page에 5개만 display됨.
+def getPage(start):
+    url = 'http://openapi.seoul.go.kr:8088/' + serviceKey + '/json/ChunmanFreeSuggestions/%d/%d' % (start, start + 4)
+    retData = getRequestUrl(url)  # [CODE 1]
+
+    if (retData == None):
+        return None
+    else:
+        return json.loads(retData)
 
 
 #[CODE 3]  "서울 열린데이터 광장" : 최대 1000개까지만 제공
